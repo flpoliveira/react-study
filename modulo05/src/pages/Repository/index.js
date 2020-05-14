50c43febcd096cd4c6561d979ef06b838735c9b2
Filipe <filipedasilvadeoliveira@gmail.com>
2020-05-14 12:59:35 -0300

    Roteamento no React

---
+++ modulo05/src/pages/Repository/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Repository() {
+  return (
+    <h1>
+     Repository
+    </h1>
+  );
+}
+
+export default Repository;
